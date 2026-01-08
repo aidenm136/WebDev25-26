8641c75509e699e2ac1315cb398efef8f1a0ed58
aidenm136 <aidenm136@nycstudents.net>
2026-01-08 18:38:13 +0000

    sss

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,21 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function showGwynn() {
+  document.getElementById("highlight").innerHTML =
+    "Tony Gwynn is the best contact hitter in MLB History.";
+}
+function showBonds() {
+  document.getElementById("highlight").innerHTML =
+    "Barry Bonds has the most home runs in MLB history";
+}
+function showJohnson() {
+  document.getElementById("highlight").innerHTML =
+    "Randy Johnson has the most strikeouts by a left-handed pitcher in MLB history";
+}
+function showYoung() {
+  document.getElementById("highlight").innerHTML =
+    "Cy Young has the most wins in MLB history";
+}
+function showCobb() {
+  document.getElementById("highlight").innerHTML =
+    "Ty Cobb has the most career hits in MLB history";
+}
